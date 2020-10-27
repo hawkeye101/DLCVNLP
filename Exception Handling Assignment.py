98cdfdec4f1f3916348196617b9ed796aeab8aee
hawkeye101 <newton.arvind@gmail.com>
2020-10-27 18:21:40 +0530

    Add files via upload

---
+++ Exception Handling Assignment.py
@@ -0,0 +1,15 @@
+# Problem 1
+def division(a,b):
+    try:
+        print(a/b)
+    except:
+        print("Exception occurred")
+        
+division(5,0)
+
+# Problem 2
+subjects = ['Americans','Indians']
+verbs = ['play','watch']
+objects = ['Baseball','Cricket']
+
+res = [i + ' ' + j + ' ' + k + '.' for i in subjects for j in verbs for k in objects]
